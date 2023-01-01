3d8f8f63da0370046be14c772d51824e74da2e07
juwel <developerjuwel792@gmail.com>
2023-01-01 18:21:12 +0600

    react textUtilitt

---
+++ l-react/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+
+function App() {
+  return (
+    <>
+    <Navbar title="Juwel"/>
+    <div className='container my-5'>
+      <TextForm title="Enter the text to anallyze"/>
+      <TextForm title="Enter the text to anallyze"/>
+    </div>
+    
+    
+    
+    </>
+ 
+  )
+}
+
+export default App;
